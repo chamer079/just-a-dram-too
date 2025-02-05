9ee23105ae93bb3c484e9aac373f22d149660a7f
chamer79 <chamer79@gmail.com>
2025-02-04 19:59:30 -0500

    updated NavBar.jsx

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,12 +1,24 @@
+import { useContext } from "react"
 import { Link } from "react-router"
+
+import { UserContext } from "../../contexts/UserContext" 
 
 
 const NavBar = () => {
+    const { user } = useContext(UserContext)
+
     return(
         <nav>
-            <ul>
-                <li><Link to='/auth/sign-up'>Sign Up</Link></li>
-            </ul>
+            {user ? (
+                <ul>
+                    <li><Link to='/whiskies'>Home</Link></li>
+                    <li><Link to='/whiskies/add'>Add New Entry</Link></li>
+                </ul>
+            ) : (
+                <ul>
+                    <li><Link to='/auth/sign-up'>Sign Up</Link></li>
+                </ul>
+            )}
         </nav>
     )
 }
