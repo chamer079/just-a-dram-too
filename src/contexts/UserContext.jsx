041bc2a04120ea0f2b873c23be955b44bf809b40
chamer79 <chamer79@gmail.com>
2025-02-04 20:07:12 -0500

    updated UserContext.jsx

--- src/contexts/UserContext.jsx
+++ src/contexts/UserContext.jsx
@@ -2,8 +2,16 @@ import { createContext, useState  } from "react"
 
 const UserContext = createContext()
 
+const getUserFormToken = () => {
+    const token = localStorage.getItem("token")
+
+    if(!token) return null
+
+    return JSON.parse(atob(token.split(".")[1])).payload
+}
+
 const UserProvider = ({ children }) => {
-    const [user, setUser] = useState(null)
+    const [user, setUser] = useState(getUserFormToken())
 
     const value = { user, setUser}
 
