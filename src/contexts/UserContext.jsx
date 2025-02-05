1378f024bbda9ab2ce6c16521e0cc4314d4c5ffa
chamer79 <chamer79@gmail.com>
2025-02-04 19:29:04 -0500

    created UserContext.jsx

---
+++ src/contexts/UserContext.jsx
@@ -0,0 +1,13 @@
+import { createContext  } from "react"
+
+const UserContext = createContext()
+
+const UserProvider = ({ children }) => {
+    return(
+        <UserContext.Provider>
+            {children}
+        </UserContext.Provider>
+    )
+}
+
+export {UserProvider}
