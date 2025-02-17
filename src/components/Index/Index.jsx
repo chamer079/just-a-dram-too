f393320f1ff51b147818fc0413701e1ebee5f8f6
chamer79 <chamer79@gmail.com>
2025-02-17 12:52:54 -0500

    styling index page

--- src/components/Index/Index.jsx
+++ src/components/Index/Index.jsx
@@ -21,13 +21,13 @@ const Index = (props) => {
             style={{ textDecoration: "none" }}
             to={`/whiskies/${whisky.id}`}
           >
-            <div className="relative flex w-full max-w-[64rem] flex-row rounded-md bg-black bg-clip-border text-white shadow-md">
-              <div className="relative m-0 w-1/4 shrink-0 overflow-hidden rounded-xl rounded-r-none bg-white bg-clip-border text-gray-700">
+            <div className="relative flex w-full max-w-[48rem] flex-row rounded-md bg-black bg-clip-border text-white shadow-md">
+              <div className="relative m-0 w-1/4 shrink-0 overflow-hidden rounded-md rounded-r-none bg-white bg-clip-border text-gray-700">
                 {!whisky.image ? (
-                  <img className="h-80 w-9/12 object-cover"    src={StockImg} alt="filler image" />
+                  <img className="max-h-70 w-9/12 object-cover" src={StockImg} alt="filler image" />
                 ) : (
                   <img
-                    className="h-80 w-9/12 object-cover"
+                    className="max-h-70 w-9/12 object-cover"
                     src={whisky.image}
                     alt={whisky.name}
                   />
@@ -39,6 +39,29 @@ const Index = (props) => {
                     <p className="mb-8 blocktext-base font-normal leading-relaxed ">{whisky.notes}</p>              
               </div>
             </div>
+
+
+
+
+
+            {/* <div className="relative flex w-full max-w-[60rem] max-h-[64rem] flex-row rounded-md bg-black bg-clip-border text-white shadow-md">
+              <div className="relative m-0 w-1/4 shrink-0 overflow-hidden rounded-med rounded-r-none bg-black bg-clip-border text-gray-700">
+                {!whisky.image ? (
+                  <img className="h-40 w-9/12 object-cover" src={StockImg} alt="filler image" />
+                ) : (
+                  <img
+                    className="h-40 w-9/12 object-scale-down"
+                    src={whisky.image}
+                    alt={whisky.name}
+                  />
+                )}
+              </div>
+              <div className="p-12">                
+                    <h2 className="mb-2 block text-white-2xl font-semibold">{whisky.name}</h2>
+                    <h2 className="mb-2 block text-2xl font-semibold leading-snug tracking-normal">{whisky.type}</h2>
+                    <p className="mb-8 blocktext-base font-normal leading-relaxed ">{whisky.notes}</p>              
+              </div>
+            </div> */}
           </Link>
         ))}
       </section>
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+// export default {
+//     future: {},
+//     purge: [],
+//     theme: {
+//         img: {
+//             width: "20vw",
+//             height: "34vh"
+
+//         }
+     
+//     },
+//     variants: {},
+//     plugins: [],
+//   }
